4b9564783e99341c3583b7b3f44757ae2dfb5816
Henrik Spolander <henrik@spolander.nu>
2017-03-27 20:30:41 +0200

    Modified to return list matching database structure

--- backend/getDataOscars.js
+++ backend/getDataOscars.js
@@ -1,48 +1,72 @@
-const cheerio = require("cheerio"),
-	req = require ("tinyreq");
+const cheerio = require('cheerio'),
+	req = require ('tinyreq');
 
-
-function scrapeOscars(url, cb) {
+module.exports = function (url, cb) {
     req(url, (err, body) => {
         if (err) { return cb(err); }
-
-        let page = cheerio.load(body)
-          , pageData = page(".wikitable")
-          , tableArray = []
-          ;
+        console.log(url);
+        let page = cheerio.load(body), 
+        	pageData = page('.wikitable'),
+        	d = new Date(),
+        	tableArray;
 
         for (var i = 0 ; pageData.length > i; i++) {
            	let table = page(pageData[i]),
            	resultlist = [],
+           	nomineeList,
            	tableTdList;
-           	if (table.find('a').first().attr('title') === "Academy Award for Best Picture") {
+           	if (table.find('a').first().attr('title') === 'Academy Award for Best Picture') {
+
+           		let date = page('.infobox.vevent').find('th:contains("Date")').next().text(),
+	           		fullDateUpdated = d.toLocaleString();
+	           		tableArray = {'ceremony_name': page('#firstHeading').text(),
+	           		'host' : page('.infobox.vevent').find('th:contains("Hosted by")').next().text(),
+	           		'year' : date.substr(date.length - 4),
+	           		'date' : date,
+	           		'last_time_accessed' : fullDateUpdated,
+	           		'award_data' : resultlist};
+
            		tableTdList = table.find('td');
+
            		for (var l =  0; l < tableTdList.length; l++) {
-           			let nomineeList = [],
-           			winner,
-           			nomineeLiItems = page(tableTdList[l]).find('li');
+           			let nomineeLiItems = page(tableTdList[l]).find('li');
+           			nomineeList = [];
+
            			for (var j =  0; j < nomineeLiItems.length; j++) {
            				nomineeList.push(page(nomineeLiItems[j]).find('a').first().text());
+                  let category = page(tableTdList[l]).find('div a').text();
+                  if (category === "") {
+                    oldOscarsCategoryTable = table.find('th');
+                    category = page(oldOscarsCategoryTable[l]).find('th a').text();
+                  }
+
            				if (page(nomineeLiItems[j]).find('b').find('a').first().text()) {
-           					winner = page(nomineeLiItems[j]).find('b').find('a').first().text();
+           					resultlist.push(
+		           				{'host': page('.infobox.vevent').find('th:contains("Hosted by")').next().text(),
+                      'year' : date.substr(date.length - 4),
+		           			 	'ceremony_name': page('#firstHeading').text(),
+		           			 	'date' : date,
+		           			 	'last_time_updated': fullDateUpdated,
+		           			 	'category': category,
+		           			 	'nominee' : page(nomineeLiItems[j]).find('a').first().text(),
+		           			 	'winner' : 1}
+                      );
+           				} else {
+	           				resultlist.push(
+		           				{'host': page('.infobox.vevent').find('th:contains("Hosted by")').next().text(),
+		           			 	'year' : date.substr(date.length - 4),
+		           			 	'ceremony_name': page('#firstHeading').text(),
+		           			 	'date' : date,
+		           			 	'last_time_updated': fullDateUpdated,
+		           			 	'category': category,
+		           			 	'nominee' : page(nomineeLiItems[j]).find('a').first().text(),
+			           			 'winner' : 0}
+                       );
            				}
            			}
-           			if (winner) {
-           				resultlist.push({"award": page(tableTdList[l]).find('div a').text(),
-           			 	"nominees" : nomineeList,
-           			 	'winner' : winner});
-           			} else {
-           				resultlist.push({"award": page(tableTdList[l]).find('div a').text(),
-           			 	"nominees" : nomineeList});
-           			}
            		};
-           	tableArray = resultlist;
+        	cb(null, resultlist);
            	}
         }
-        cb(null, tableArray);
     });
 }
-
-scrapeOscars("https://en.wikipedia.org/w/index.php?title=89th_Academy_Awards&offset=20170227051636&limit=500&action=history", (err, data) => {
-    console.log(err || data);
-});
